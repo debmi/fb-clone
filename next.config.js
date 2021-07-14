a30f40799616c9ffc3225fc15509c334f02d130c
debmi <87399148+debmi@users.noreply.github.com>
2021-07-14 01:09:48 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    firebaseConfig: {
+    apiKey: "AIzaSyAyVk7gK3ZL8RJ1vDuMm5pwJKDTJKQZbZ0",
+    authDomain: "contactlist-163b0.firebaseapp.com",
+    databaseURL: "https://contactlist-163b0.firebaseio.com",
+    projectId: "contactlist-163b0",
+    storageBucket: "",
+    messagingSenderId: "593204922920"
+  }
+  },
+}
