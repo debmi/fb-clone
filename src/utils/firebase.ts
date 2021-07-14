9ff280062e28ec6db14415634a1fec3db72e9cdf
debmi <87399148+debmi@users.noreply.github.com>
2021-07-15 00:31:09 +0100

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import firebaseConfig from '../config.json';
+import firebaseConfig from '../config.js';
 
 firebase.initializeApp(firebaseConfig);
 
