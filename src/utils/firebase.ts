40d93871eb4cdb7f4b9cd9c4110367637f184cd4
debmi <87399148+debmi@users.noreply.github.com>
2021-07-15 00:23:16 +0100

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,21 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
 
-import config from '../config.json';
+// import config from '../config.json';
 
-const firebaseConfig = config.firebase;
+// const firebaseConfig = config.firebase;
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-export type Auth = firebase.auth.Auth;
-export type Firestore = firebase.firestore.Firestore;
-export type GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
-export type DocumentData = firebase.firestore.DocumentData;
-export type UnsubscribeFn = () => void;
-export type DocumentSnapshot<T> = firebase.firestore.DocumentSnapshot<T>;
-export type SnapshotFn = (snapshot: DocumentSnapshot<DocumentData>) => void;
+// export type Auth = firebase.auth.Auth;
+// export type Firestore = firebase.firestore.Firestore;
+// export type GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+// export type DocumentData = firebase.firestore.DocumentData;
+// export type UnsubscribeFn = () => void;
+// export type DocumentSnapshot<T> = firebase.firestore.DocumentSnapshot<T>;
+// export type SnapshotFn = (snapshot: DocumentSnapshot<DocumentData>) => void;
 
-export const firestore: Firestore = firebase.firestore();
-export const auth: Auth = firebase.auth();
-export const provider: GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+// export const firestore: Firestore = firebase.firestore();
+// export const auth: Auth = firebase.auth();
+// export const provider: GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
