8db94f927ffb8436bdb19f4b9e1a94db0566982b
debmi <87399148+debmi@users.noreply.github.com>
2021-07-15 00:58:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router';
 
-import { Login } from './pages';
-import { PublicRoute, PrivateRoute } from './components';
-import { AuthProvider } from './context/auth';
-import './App.scss';
-import { ROUTES } from './utils/constants';
+import './Login.scss';
+import fbNameLogo from '../../assets/fbNameLogo.png';
 
 function App(): React.ReactElement {
+
+  const onSignIn = async () => {
+    try {
+      await signin();
+      history.push(ROUTES.HOME);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <Router>
-      <AuthProvider>
-        <Switch>
-          <div className='App'>
-            <PublicRoute path={ROUTES.SIGNIN} component={Login} exact />
-          </div>
-        </Switch>
-      </AuthProvider>
-    </Router>
+    <div className='login'>
+      <div className='logo'>
+        <img src={fbNameLogo} alt='name logo' />
+        <h3>Facebook helps you connect and share with the people in your life.</h3>
+      </div>
+      <div className='loginCard'>
+        <div className='card'>
+          <span className='title'>Sign In</span>
+          <p>Sign in with your Google account</p>
+          <Button className='button' type='submit' onClick={onSignIn}>
+            SIGN IN
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default App;
+
